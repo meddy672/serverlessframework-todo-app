9e56358d299bd720bc7804fc453d0c7ee23f2441
Matthew Eddy <meddy672@gmail.com>
2021-05-22 12:12:59 -0400

    added functionality for create todo and updated serverless.yml

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,33 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
+import * as uuid from 'uuid'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const todoId = uuid.v4().toString()
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const createTodo = {
+    id: todoId,
+    ...newTodo
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: createTodo
+  }).promise();
+
+  return {
+    statusCode: 201,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({ createTodo })
+  };
 }
