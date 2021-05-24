afa78e15ecaa6599753080d668130a3df4de990b
Matthew Eddy <meddy672@gmail.com>
2021-05-24 14:00:42 -0400

    added functionality for updating todos

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,43 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODO_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const result = await docClient.update({
+    TableName: todosTable,
+    Key: {
+      todoId
+    },
+    UpdateExpression: "set #name = :name, dueDate=:dueDate, done=:done",  ExpressionAttributeValues:{        
+      ":name": updatedTodo.name,        
+      ":dueDate": updatedTodo.dueDate,        
+      ":done": updatedTodo.done    
+    },    
+    ExpressionAttributeNames: {"#name": "name"},    ReturnValues:"UPDATED_NEW"  }).promise()
+
+  if (result) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(result)
+    }
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
 }
