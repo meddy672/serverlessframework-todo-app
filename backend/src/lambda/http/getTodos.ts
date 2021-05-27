7fead2d2daa92b758cb8ac3a0af9588a8365fe85
Matthew Eddy <meddy672@gmail.com>
2021-05-27 15:12:39 -0400

    get todos is now working using query by todoId

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -6,11 +6,16 @@ const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODO_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  
+  const todoId = event.pathParameters.todoId
   console.log('Processing event:', event)
 
   const result = await docClient.query({
-      TableName: todosTable
+    TableName: todosTable,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    },
+    ScanIndexForward: false
   }).promise()
 
   const items = result.Items
