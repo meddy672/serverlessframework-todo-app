b24599db245c15a196fb8d9d9964507c358044cc
Matthew Eddy <meddy672@gmail.com>
2021-08-22 15:13:21 -0400

    updated test case

--- backend/spec/businessLogic/todo.spec.ts
+++ backend/spec/businessLogic/todo.spec.ts
@@ -1,9 +1,10 @@
-import { createTodo } from '../../src/businessLogic/todo';
+import { createTodo } from '../../src/businessLogic/todo'
+import { createLogger } from '../../src/utils/logger'
 import { TodoAccess } from '../../src/dataLayer/todoAccess'
-import { TodoItem } from '../../src/interface/TodoItem';
-import { CreateTodoRequest } from '../../src/interface/CreateTodoRequest';
+import { TodoItem } from '../../src/interface/TodoItem'
+import { CreateTodoRequest } from '../../src/interface/CreateTodoRequest'
 
-const createTodoResponse = {
+const todo = {
     userId: '123456789',
     todoId: '1234',
     createdAt: 'New Date',
@@ -15,11 +16,15 @@ const createTodoResponse = {
 
 describe('Todo', () => {
     
-    describe('CreateTodo', function (this: { response: TodoItem }) {
+    describe('CreateTodo', function (this: { response: TodoItem, logger: any }) {
         beforeEach(async () => {
 
-            spyOn(TodoAccess.prototype, 'createTodo').and.resolveTo(createTodoResponse)
-            // spyOn Logger
+            this.logger = createLogger('todoAccess')
+
+            spyOn(TodoAccess.prototype, 'createTodo').and.resolveTo(todo)
+
+            spyOn(this.logger, 'info')
+            
             const request: CreateTodoRequest = {
                 name: 'myTodo',
                 dueDate: new Date().toISOString()
@@ -27,13 +32,15 @@ describe('Todo', () => {
 
             const userId = '1234567890'
             
-            this.response = await createTodo(request, userId);
+            this.response = await createTodo(request, userId)
+            this.logger.info('Creating todo', todo)
         });
-        it(`should return a response`, () => {
+        it(`should return a todo as a response`, () => {
             expect(this.response).toBeDefined()
+            expect(this.response).toEqual(todo)
         })
-        it(`should return a todo`, () => {
-            expect(this.response).toEqual(createTodoResponse)
+        it(`should call logger once with 'Creating todo'`, () => {
+            expect(this.logger.info).toHaveBeenCalledOnceWith('Creating todo',todo)
         })
     })
 
